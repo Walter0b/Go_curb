8f45c6b2df74de593cd004db89246f13fc9c5156
walter0b <jakinjoram5@outlook.com>
2023-10-28 12:37:34 +0100

    Update project name

--- go.mod
+++ go.mod
@@ -1,11 +1,10 @@
-module gocrub
+module go_curb
 
 go 1.21.3
 
 require (
 	github.com/gin-gonic/gin v1.9.1
 	github.com/joho/godotenv v1.5.1
-	github.com/lib/pq v1.10.9
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
--- routes/routers.go
+++ routes/routers.go
@@ -1,7 +1,7 @@
 package routes
 
 import (
-	"gocrub/tableTypes"
+	"go_curb/tableTypes"
 	"log"
 	"net/http"
 
@@ -10,29 +10,29 @@ import (
 )
 
 func CustomerRoutes(r *gin.Engine, db *gorm.DB) {
-    r.GET("/", func(c *gin.Context) {
-        var customers []tableTypes.Customer
-        if err := db.Find(&customers).Error; err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-            return
-        }
-        c.JSON(http.StatusOK, customers)
-    })
+	r.GET("/", func(c *gin.Context) {
+		var customers []tableTypes.Customer
+		if err := db.Find(&customers).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, customers)
+	})
 
-    r.POST("/customers", func(c *gin.Context) {
-        var customer tableTypes.Customer
-        if err := c.ShouldBindJSON(&customer); err != nil {
-            log.Println(customer)
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
+	r.POST("/customers", func(c *gin.Context) {
+		var customer tableTypes.Customer
+		if err := c.ShouldBindJSON(&customer); err != nil {
+			log.Println(customer)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-        if err := db.Create(&customer).Error; err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-            return
-        }
+		if err := db.Create(&customer).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
-        c.JSON(http.StatusCreated, customer)
-    })
-    
+		c.JSON(http.StatusCreated, customer)
+	})
+
 }
